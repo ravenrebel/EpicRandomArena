140bc0b0d009269285eba7a3cfedec7d1d5ddb51
RudeBit4 <cat.in.tardis@gmail.com>
2019-11-14 16:51:21 +0200

    Add style trigger on turn to hide or not back card Add drop cards and players decks

--- EpicRandomArena/Models/Deck.cs
+++ EpicRandomArena/Models/Deck.cs
@@ -14,7 +14,7 @@ namespace EpicRandomArena.Models
         {
             cards = new ObservableCollection<Card>();
             cards.Add(new Card("Ilona", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 9, 4, 11));
-            cards.Add(new Card("Anastasia", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 7, 1, 12));
+            cards.Add(new Card("Anastasia", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 9, 1, 12));
             cards.Add(new Card("Tony Stark", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 13, 5, 15));
             cards.Add(new Card("Batman", "/EpicRandomArena;component/Graphics/CardFolder/batman.jpg", 12, 10, 6));
             cards.Add(new Card("Darth Vader", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 4, 14, 2));
@@ -31,7 +31,7 @@ namespace EpicRandomArena.Models
             cards.Add(new Card("Diakoniuk", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 5, 4, 13));
             cards.Add(new Card("Iashchuk Xandrovich", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 7, 5, 13));
             cards.Add(new Card("Loki", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 14, 4, 8));
-            cards.Add(new Card("Hapko", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 9, 2, 13));
+            cards.Add(new Card("Hapko", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 7, 2, 13));
             cards.Add(new Card("Half of Truhan", "/EpicRandomArena;component/Graphics/CardFolder/Ilona.png", 1, 3, 8));
         }
 
--- EpicRandomArena/ViewModels/LevelToBrushConverter.cs
+++ EpicRandomArena/ViewModels/LevelToBrushConverter.cs
@@ -10,9 +10,9 @@ namespace EpicRandomArena.ViewModels
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if ((Levels)value == Levels.High) return new SolidColorBrush(Color.FromArgb(255, 140, 30, 200));
-            else if ((Levels)value == Levels.Middle) return new SolidColorBrush(Color.FromArgb(255, 240, 30, 0)); 
-            else return new SolidColorBrush(Color.FromArgb(255, 10, 30, 200));
+            if ((Levels)value == Levels.High) return new SolidColorBrush(Color.FromArgb(255, 197, 92, 252));
+            else if ((Levels)value == Levels.Middle) return new SolidColorBrush(Color.FromArgb(255, 228, 164, 254)); 
+            else return new SolidColorBrush(Color.FromArgb(255, 244, 228, 250));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
