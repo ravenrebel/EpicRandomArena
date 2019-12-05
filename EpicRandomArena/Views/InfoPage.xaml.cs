8bf42e035f66314b88d920f2af2392ae73f5935a
RudeBit4 <cat.in.tardis@gmail.com>
2019-12-05 22:57:28 +0200

    Add rules &history button copyright & developers

--- EpicRandomArena/Views/InfoPage.xaml.cs
+++ EpicRandomArena/Views/InfoPage.xaml.cs
@@ -30,5 +30,15 @@ namespace EpicRandomArena.Views
             ExitGameWindow exitGameWindow = new ExitGameWindow(Window.GetWindow(this));
             exitGameWindow.Show();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextInfo.Text = "Not a long time ago in a galaxy not very far away, scientists were searching for the Higgs boson. The Large Hadron Collider generated a large number of small black holes that became portals to many parallel Universes. This is how humanity met with foreigners and the First Universes War began. Heroes and villains made their choice: peaceful coexistence or the struggle for resources.";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TextInfo.Text = "Each card has three characteristics: intelligence, physical strength, stealth. Each characteristic is in one of the ranges: low level: 1-5, middle: 6 - 10, high: 11 - 15.If the characteristics are the same level, the card is stronger when the value is greater. Low → strong → medium → low. Players get one card at a time.One of the players chooses one of the characteristics, if it 'hits' the same on the opponent's card, its card is dropped and the winning card gets mixed back into player's deck. If the characteristics are the same, two cards are randomly mixed into the deck.The one who has no cards left loses.";
+        }
     }
 }
