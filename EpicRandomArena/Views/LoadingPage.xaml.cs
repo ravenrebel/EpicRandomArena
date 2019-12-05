0d0b23c8b461c88da8f18b0a4bd1286e89c45bb5
anastasia.baklayeva <nasty199961@gmail.com>
2019-12-05 23:12:36 +0200

    Create spinner in Loading Page

--- EpicRandomArena/Views/LoadingPage.xaml.cs
+++ EpicRandomArena/Views/LoadingPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,40 @@ namespace EpicRandomArena.Views
         public LoadingPage()
         {
             InitializeComponent();
+            DrawCanvas();
+            DoubleAnimation a = new DoubleAnimation();
+            a.From = 0;
+            a.To = 360;
+            a.RepeatBehavior = RepeatBehavior.Forever;
+            a.SpeedRatio = 2;
+
+            Spin.BeginAnimation(RotateTransform.AngleProperty, a);
+        }
+
+        void DrawCanvas()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                Line line = new Line()
+                {
+                    X1 = 50,
+                    X2 = 50,
+                    Y1 = 0,
+                    Y2 = 20,
+                    StrokeThickness = 7,
+                    Stroke = Brushes.White,
+                    Width = 100,
+                    Height = 200
+                };
+                line.VerticalAlignment = VerticalAlignment.Center;
+                line.HorizontalAlignment = HorizontalAlignment.Center;
+                line.RenderTransformOrigin = new Point(.5, .5);
+                line.RenderTransform = new RotateTransform(i * 30);
+                line.Opacity = (double)i / 12;
+
+                SpinnerCanvas.Children.Add(line);
+
+            }
         }
     }
 }
